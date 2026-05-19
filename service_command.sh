5ac6a1dea768cf1c0aa796dd2d4d60aa28406fc4
Kirill Korablin <kkorablin@yandex.ru>
2026-05-19 10:15:00 +0300

    service command to pull/push

---
+++ service_command.sh
@@ -0,0 +1,3 @@
+git pull git@github.com:sherbettt/Banana-Pi-OpenWrt-Router.git
+git push git@gitflic.ru:kkorablin/banana-pi-openwrt-router.git
+git push git@gitverse.ru:sherbettt/Banana-Pi-OpenWrt-Router.git
